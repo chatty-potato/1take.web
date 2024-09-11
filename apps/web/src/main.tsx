ebb2b24c1421b8e480c83fd8b337936ac428aae3
nerdchanii <nerdchanii@gmail.com>
2024-09-11 17:11:55 +0900

    feat(web): add Router in React-dom

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+import "./index.css";
+import router from "./routes";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
