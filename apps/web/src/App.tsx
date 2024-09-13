c46c03bbb5bd2afef8e3a02fb5809b4764c948cf
nerdchanii <nerdchanii@gmail.com>
2024-09-13 11:06:19 +0900

    fix(web): relocate of RouterProvider

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,40 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { AuthApis } from "@1take/apis";
-import { Button, Input } from "@1take/ui";
+import router from "./routes";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <Button  label="버튼" backgroundColor="white"/>
-        <Input />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <button onClick={AuthApis.getUser}>Login</button>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
 import "./index.css";
-import router from "./routes";
-import { RouterProvider } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
