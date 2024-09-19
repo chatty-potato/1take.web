d20facfefed57f7b0ca7a8e8951d94d1fde0c4d9
rheefine <rheefine@gmail.com>
2024-09-19 22:59:20 +0900

    feat: add eslint & prettier

--- ui/vitest.setup.ts
+++ ui/vitest.setup.ts
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
-import React from "react";
 
 afterEach(() => {
   cleanup();
