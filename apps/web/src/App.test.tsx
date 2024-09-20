7b86905f6aa22b99bbc3ccfdf72a80cbad6c2562
nerdchanii <nerdchanii@gmail.com>
2024-09-20 14:13:24 +0900

    setup(eslint): add rules for vitest

--- apps/web/src/App.test.tsx
+++ apps/web/src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-describe("App 컴포넌트를 렌더링 하라", () => {
-  test("App 컴포넌트를 렌더링", () => {
+describe('App 컴포넌트를 렌더링 하라', () => {
+  test('App 컴포넌트를 렌더링', () => {
     render(<App />);
-    expect(screen.getByText("Hello World")).toBeInTheDocument();
+    expect(screen.getByText('Hello World')).toBeInTheDocument();
   });
 });
--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,32 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import vitest from '@vitest/eslint-plugin';
 
 export default [
   {
+    ignores: ['.yarn/**', '**/.storybook/**'],
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
   },
   {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off', //  Vite.js handle this automatically
+    },
+  },
+  {
+    files: ['**/**/**.test.**'], // or any other pattern
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
+      'vitest/max-nested-describe': ['error', { max: 3 }], // you can also modify rules' behavior using option like this
+    },
+  },
 ];
