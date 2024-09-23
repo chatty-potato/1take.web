94c4a035e6b4a5b2fe0a641909536e9cf3d3988c
rheefine <rheefine@gmail.com>
2024-09-23 16:04:47 +0900

    fix: api 경로 상수화

--- libs/apis/src/utils/config.ts
+++ libs/apis/src/utils/config.ts
@@ -1,17 +1,26 @@
-const API_URL = "http://localhost:4000/api" 
+import { Q } from 'vitest/dist/reporters-B7ebVMkT';
+
+const API_URL = 'http://localhost:4000/api/v1';
 // || (import.meta.env.VITE_API_URL as string) ; // VITE 패키지의 위치에 대한 고려 필요
 const DEFAULT_TIME_OUT = 1000 * 10; // 10초
 const AUTH_UN_NEED_URL: RegExp[] = [/\/auth\/login/];
-const AUTH_TOKEN_TYPE = "Bearer";
-
+const AUTH_TOKEN_TYPE = 'Bearer';
 
 const API_RESOURCE = {
-  QNA: '/qna',
   AUTH: '/auth',
-  INTERVIEW: '/interview',
   PROFILE: '/profile',
-  BOOOKMARK: '/bookmark',
+  CATEGORY: '/category',
+  INTERVIEWS: '/interviews',
+  QUESTIONS: '/questions',
+  REPORTS: '/reports',
+  BOOOKMARKS: '/bookmarks',
+  QNA: '/qna',
+};
 
-}
-
-export { API_URL, DEFAULT_TIME_OUT, AUTH_UN_NEED_URL, AUTH_TOKEN_TYPE, API_RESOURCE };
+export {
+  API_URL,
+  DEFAULT_TIME_OUT,
+  AUTH_UN_NEED_URL,
+  AUTH_TOKEN_TYPE,
+  API_RESOURCE,
+};
