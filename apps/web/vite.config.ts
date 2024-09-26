dcb773a7e817ce84031f8db9c798607cbd7670f5
nerdchanii <nerdchanii@gmail.com>
2024-09-26 16:01:42 +0900

    feat(web): tailwindcss 추가(#21)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 /// <reference types="vitest" />
 // https://vitejs.dev/config/
