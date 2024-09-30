8a7de73b2450701fde92392cc36521cbe62e37c4
nerdchanii <nerdchanii@gmail.com>
2024-09-30 10:54:37 +0900

    fix: 헤더의 로고를 누르면 홈으로 이동(#21)

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   // TODO: 전역 상태로 로그인을 관리합니다. 로그인기능을 구현 후, 상태를 추가하면서 해당 부분을 고쳐주세요.
@@ -7,11 +8,13 @@ const Header = () => {
 
   return (
     <header className="m-0 p-2.5 pr-6 flex justify-between items-center h-12">
-      <img
-        src={Logo}
-        alt="logo"
-        className="w-auto h-6"
-      />
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          className="w-auto h-6"
+        />
+      </Link>
       {/* // TODO: 로그인 상태를 전역 상태로 관리하고, 로그인 상태에 따라 버튼을 렌더링하도록 합니다.*/}
       {!isLogin && (
         <button
